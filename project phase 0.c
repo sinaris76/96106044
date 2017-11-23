8cabdac4abd99ddf6f747e1d10caf07231d37394
leily goli <leilygoli100@yahoo.com>
2017-11-23 23:44:05 +0330

    added a function to count the points and made some corrections in getting the input and finding pacman's place(for blocks and when exitting the page)

--- project phase 0.c
+++ project phase 0.c
@@ -1,29 +1,78 @@
 #include <stdio.h>
 #include <stdlib.h>
-void pacplace(int *PacNowX, int *PacNowY, int PacDir){
-		if(PacDir==1)
-		*PacNowY=*PacNowY-1;
-		if(PacDir==2)
-		*PacNowX=*PacNowX+1;
-		if(PacDir== 3)
-		*PacNowY=*PacNowY+1;
-		if(PacDir==4)
-		*PacNowX=*PacNowX-1;	
+
+int pacplace(int *PacNowX, int *PacNowY, int PacDir,char plan[100][100],int line, int column){
+	int y=*PacNowY,x=*PacNowX, flag1=0;
+		if(PacDir==1){
+			*PacNowY=*PacNowY-1;
+			if(*PacNowY<0)
+			*PacNowY=line-1;}
+		if(PacDir==2){
+			*PacNowX=*PacNowX+1;
+			if(*PacNowX>column-1)
+			*PacNowX=0;}
+		if(PacDir== 3){
+			*PacNowY=*PacNowY+1;
+			if(*PacNowY>line-1)
+			*PacNowY=0;}
+		if(PacDir==4){
+			*PacNowX=*PacNowX-1;
+			if(*PacNowX<0)
+			*PacNowX=column-1;}
+		if(plan[*PacNowY][*PacNowX]=='#'){
+			*PacNowY=y;	
+			*PacNowX=x;
+			flag1=1;}
+	return flag1;
+}
+int pacpoints(int *points,int* heart,int PacNowX, int PacNowY, char plan[100][100], int PinkyNowX,int PinkyNowY,int PinkyType, int BlinkyNowX,int BlinkyNowY,int BlinkyType,int InkyNowX,int InkyNowY,int InkyType,int ClydeNowX,int ClydeNowY,int ClydeType){
+	int flag=0;
+	if (plan[PacNowY][PacNowX]=='*'){
+		*points=*points+1;}
+	if (plan[PacNowY][PacNowX]=='^'){
+		*points=*points+20;}
+	if (plan[PacNowY][PacNowX]=='O'){
+		*points=*points+50;}
+	if(PacNowX==BlinkyNowX && PacNowY==BlinkyNowY){
+		if(BlinkyType){
+		*heart=*heart-1;
+		flag=1;}
+		else
+		*points=*points+50;}
+	if(PacNowX==PinkyNowX && PacNowY==PinkyNowY){
+		if(PinkyType){
+		*heart=*heart-1;
+		flag=1;}
+		else
+		*points=*points+50;}
+	if(PacNowX==InkyNowX && PacNowY==InkyNowY){
+		if(InkyType){
+		*heart=*heart-1;
+		flag=1;}
+		else
+		*points=*points+50;}
+	if(PacNowX==ClydeNowX && PacNowY==ClydeNowY){
+		if(ClydeType){
+		*heart=*heart-1;
+		flag=1;}
+		else
+		*points=*points+50;}
+		return flag;
 }
 int main() {
-	int line,column,minute,second, points, heart,PacDir,PacStartX,PacStartY,PacNowX,PacNowY,BlinkyDir,BlinkyStartX,BlinkyStartY,BlinkyNowX,BlinkyNowY,BlinkyType;
+	int line,column,minute,second, points, heart,PacDir,PacStartX,PacStartY,PacNowX,PacNowY,BlinkyDir,BlinkyStartX,BlinkyStartY,BlinkyNowX,BlinkyNowY,BlinkyType,BlinkyTime;
 	
-	int PinkyDir,PinkyStartX,PinkyStartY,PinkyNowX,PinkyNowY,PinkyType,InkyDir,InkyStartX,InkyStartY,InkyNowX,InkyNowY,InkyType,ClydeDir,ClydeStartX,ClydeStartY,ClydeNowX,ClydeNowY,ClydeType;
+	int PinkyDir,PinkyStartX,PinkyStartY,PinkyNowX,PinkyNowY,PinkyType,PinkyTime,InkyDir,InkyStartX,InkyStartY,InkyNowX,InkyNowY,InkyType,InkyTime,ClydeDir,ClydeStartX,ClydeStartY,ClydeNowX,ClydeNowY,ClydeType,ClydeTime;
 	
 	char plan[100][100];
 	
-	int i,j;
+	int i,j,flag,flag1;
 	
 	scanf("%d %d\n",&line,&column);
 	
 	for(i=0 ; i<line ; i++){
 		for(j=0 ; j<column ; j++){
-			scanf("%c",&plan[line][column]);
+			scanf("%c",&plan[i][j]);
 		}
 		scanf("\n");}
 		
@@ -31,16 +80,34 @@ int main() {
 	scanf("\n");
 	scanf("pacman: %d %d (%d,%d) (%d,%d)",&PacDir,&heart,&PacStartX,&PacStartY,&PacNowX,&PacNowY);
 	scanf("\n");
-	scanf("blinky: %d %d (%d,%d) (%d,%d)",&BlinkyDir,&BlinkyType,&BlinkyStartX,&BlinkyStartY,&BlinkyNowX,&BlinkyNowY);
+	scanf("blinky: %d %d ",&BlinkyDir,&BlinkyType);
+	if(BlinkyType==0)
+		scanf("%d ",&BlinkyTime);
+	scanf("(%d,%d) (%d,%d)",&BlinkyStartX,&BlinkyStartY,&BlinkyNowX,&BlinkyNowY);
 	scanf("\n");
-	scanf("pinky: %d %d (%d,%d) (%d,%d)",&PinkyDir,&PinkyType,&PinkyStartX,&PinkyStartY,&PinkyNowX,&PinkyNowY);
+	scanf("pinky: %d %d ",&PinkyDir,&PinkyType);
+	if(PinkyType==0)
+		scanf("%d ",&PinkyTime);
+	scanf("(%d,%d) (%d,%d)",&PinkyStartX,&PinkyStartY,&PinkyNowX,&PinkyNowY);
 	scanf("\n");
-	scanf("clyde: %d %d (%d,%d) (%d,%d)",&ClydeDir,&ClydeType,&ClydeStartX,&ClydeStartY,&ClydeNowX,&ClydeNowY);
+	scanf("clyde: %d %d ",&ClydeDir,&ClydeType);
+	if(ClydeType==0)
+		scanf("%d ",&ClydeTime);
+	scanf("(%d,%d) (%d,%d)",&ClydeStartX,&ClydeStartY,&ClydeNowX,&ClydeNowY);
 	scanf("\n");
-	scanf("inky: %d %d (%d,%d) (%d,%d)",&InkyDir,&InkyType,&InkyStartX,&InkyStartY,&InkyNowX,&InkyNowY);
+	scanf("inky: %d %d ",&InkyDir,&InkyType);
+	if(InkyType==0)
+		scanf("%d ",&InkyTime);
+	scanf("(%d,%d) (%d,%d)",&InkyStartX,&InkyStartY,&InkyNowX,&InkyNowY);
 		
-pacplace(&PacNowX,&PacNowY,PacDir);
-
-printf("(%d,%d)",PacNowX,PacNowY);
+   flag1=pacplace(&PacNowX,&PacNowY,PacDir,plan,line,column);
+   if (flag1==0){
+   flag=pacpoints(&points, &heart,PacNowX,PacNowY,plan,PinkyNowX,PinkyNowY,PinkyType,BlinkyNowX,BlinkyNowY,BlinkyType,InkyNowX,InkyNowY,InkyType,ClydeNowX,ClydeNowY,ClydeType);
+	if(flag==1 && heart!=0){
+		PacNowX=PacStartX;
+		PacNowY=PacStartY;
+	}}
+printf("(%d,%d)\n",PacNowX,PacNowY);
+printf("%d",points);
 	return 0;
 }
